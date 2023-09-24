956bf9963db343d7a27349ca9b0ca4731c6a035f
guehara-code <gil_uehara@yahoo.com.br>
2023-09-24 19:59:49 -0300

    more studies

---
+++ 03-05-LiskovSubstitution/src/Bus.java
@@ -0,0 +1,12 @@
+
+public class Bus extends Vehicle {
+    int getSpeed() {
+        return 0;
+    }
+    int getCubicCapacity() {
+        return 0;
+    }
+    String getEmergencyExitLoc() {
+        return null;
+    }
+}
---
+++ 03-05-LiskovSubstitution/src/Car.java
@@ -0,0 +1,12 @@
+
+public class Car extends Vehicle {
+    int getSpeed() {
+        return 0;
+    }
+    int getCubicCapacity() {
+        return 0;
+    }
+    boolean sHatchBack() {
+        return true;
+    }
+}
