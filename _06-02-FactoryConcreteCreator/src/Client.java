ef5b389c8d557220595b7a735d970ce3dab2f1cc
guehara-code <gil_uehara@yahoo.com.br>
2023-09-26 07:20:26 -0300

    more studies

---
+++ _06-02-FactoryConcreteCreator/src/Client.java
@@ -0,0 +1,19 @@
+public class Client {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        // get an object of Circle and call its draw method.
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+        shape1.draw();
+
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+
+        shape2.draw();
+
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+
+        shape3.draw();
+    }
+
+}
