c38931c37f2cf382d9d2fcb091ed6bac9b4524b7
guehara-code <gil_uehara@yahoo.com.br>
2023-10-02 09:26:30 -0300

    more studies

---
+++ _12-04-AdapterChallenge/src/AdvancedMediaPlayerAdapter.java
@@ -0,0 +1,13 @@
+public class AdvancedMediaPlayerAdapter implements MediaPlayerInterface {
+    AdvancedMediaPlayerInterface advancedMusicPlayer;
+
+    public AdvancedMediaPlayerAdapter(AdvancedMediaPlayerInterface myMediaPlayer) {
+        advancedMusicPlayer = myMediaPlayer;
+    }
+
+    @Override
+    public void play(String audioType, String filename) {
+        advancedMusicPlayer.loadFilename(filename);
+        advancedMusicPlayer.listen();
+    }
+}
---
+++ _12-04-AdapterChallenge/src/MediaPlayerInterface.java
@@ -0,0 +1,17 @@
+public interface MediaPlayerInterface {
+    public void play(String audioType, String fileName);
+}
+
+class AudioPlayer implements MediaPlayerInterface {
+    @Override
+    public void play(String audioType, String filename) {
+        if(audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + filename);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported.");
+        }
+    }
+
+}
+
+
