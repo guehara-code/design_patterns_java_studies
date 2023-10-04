03989a94849f1d9adc5d3c43117092448bb35617
guehara-code <gil_uehara@yahoo.com.br>
2023-10-04 09:11:55 -0300

    more studies

---
+++ _15-03-DecoratorChallenge/src/Shape.java
@@ -0,0 +1,17 @@
+public interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ _15-03-DecoratorChallenge/src/ShapeDecorator.java
@@ -0,0 +1,27 @@
+abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw() {
+        decoratedShape.draw();
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape) {
+        System.out.println("Border Color: Red");
+    }
+}
