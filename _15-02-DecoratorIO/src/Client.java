4cf93a25a46e513e8e21c7b94e53574392bf3c34
guehara-code <gil_uehara@yahoo.com.br>
2023-10-04 08:48:44 -0300

    more studies

---
+++ _15-02-DecoratorIO/src/Client.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args) {
+        int c;
+
+        StringBuffer stringBuffer = new StringBuffer("Jason Fedin aaaBBBcccDDD");
+        byte[] bytes = stringBuffer.toString().getBytes();
+
+        try {
+            InputStream in = new LowerCaseInputStream(new ByteArrayInputStream(bytes));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
