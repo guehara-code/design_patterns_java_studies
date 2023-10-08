42310486552cbbcca17cf042b4952c4b474c7f4f
guehara-code <gil_uehara@yahoo.com.br>
2023-10-08 20:49:10 -0300

    more studies

---
+++ _22-01-Command/src/FileInvoker.java
@@ -0,0 +1,11 @@
+class FileInvoker {
+    public Command command;
+
+    public FileInvoker(Command c) {
+        command = c;
+    }
+
+    public void execute() {
+        command.execute();
+    }
+}
