5c8fc995139d49a4ff607c4269384b620a1a3474
guehara-code <gil_uehara@yahoo.com.br>
2023-10-09 20:14:43 -0300

    more studies

---
+++ _23-02-InterpreterChallenge/src/Expression.java
@@ -0,0 +1,17 @@
+public interface Expression {
+    public boolean interpret(Context context);
+}
+
+class TerminalExpression implements Expression {
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(Context context) {
+        return context.getResult(data);
+    }
+
+}
