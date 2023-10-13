373290404f9648d15342f865b8548ae878611ef6
guehara-code <gil_uehara@yahoo.com.br>
2023-10-13 14:56:22 -0300

    more studies

---
+++ _29-02-StrategyChallenge/src/Context.java
@@ -0,0 +1,5 @@
+public class Context {
+    public int executeStrategy(Strategy strategy, int num1, int num2) {
+        return strategy.performOperation(num1, num2);
+    }
+}
---
+++ _29-02-StrategyChallenge/src/Strategy.java
@@ -0,0 +1,25 @@
+interface Strategy {
+    public int performOperation(int num1, int num2);
+}
+
+class OperationAdd implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+class OperationSubtract implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class OperationMultiply implements Strategy {
+    @Override
+    public int performOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
+
