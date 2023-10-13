73f9bb052b468a089e75e8a9703d66a471bd4816
guehara-code <gil_uehara@yahoo.com.br>
2023-10-13 16:49:35 -0300

    more studies

---
+++ _30-01-Template/src/Client.java
@@ -0,0 +1,15 @@
+import java.lang.management.GarbageCollectorMXBean;
+
+public class Client {
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+
+        // using template method
+        houseType.buildHouse();
+
+        System.out.println("**************");
+
+        houseType = new GlassHouse();
+        houseType.buildHouse();
+    }
+}
