ae2c0ab76b757d1bf7fb70272fe76444946d4abb
guehara-code <gil_uehara@yahoo.com.br>
2023-10-15 07:48:56 -0300

    more studies

---
+++ _31-01-Visitor/src/ItemElement.java
@@ -0,0 +1,49 @@
+public interface ItemElement {
+    public int accept (ShoppingCartVisitor visitor);
+}
+
+class Book implements ItemElement {
+    private int price;
+    private String isbnNumber;
+
+    public Book(int cost, String isbn) {
+        this.price = cost;
+        this.isbnNumber = isbn;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    @Override
+    public int accept(ShoppingCartVisitor visitor) {
+        return visitor.visit(this);
+    }
+}
+
+class Fruit implements ItemElement {
+    private int pricePerKg;
+    private int weight;
+    private String name;
+
+    public int getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int accept(ShoppingCartVisitor visitor) {
+        return visitor.visit(this);
+    }
+}
