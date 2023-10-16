5a171f372ef519d69671247e789501c1ce4f5f54
guehara-code <gil_uehara@yahoo.com.br>
2023-10-16 09:27:04 -0300

    more studies

---
+++ _31-01-VisitorChallenge/src/Client.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Client {
+
+    private static ArrayList<Visitable> items = new ArrayList<Visitable>();
+
+    public static double calculatePostage(Visitor visitor) {
+        // iterate through all items
+        for(Visitable item: items) {
+            item.accept(visitor);
+        }
+        double postage = visitor.getTotalPostage();
+        return postage;
+    }
+
+    public static void main(String[] args) {
+        Visitable myBook = new Book(8.52, 1.05);
+        Visitable myCD = new CD(18.52, 3.05);
+        Visitable myDVD = new DVD(6.53, 4.02);
+
+        //add to array list
+        items.add(myBook);
+        items.add(myCD);
+        items.add(myDVD);
+
+        Visitor visitor = new USPostageVisitor();
+        double myPostage = calculatePostage(visitor);
+        System.out.println("The total postage for my items is: " + myPostage);
+
+        visitor = new SouthAmericaPostageVisitor();
+        myPostage = calculatePostage(visitor);
+        System.out.println("The total postage for my items is: " + myPostage);
+
+    }
+}
---
+++ _31-01-VisitorChallenge/src/Visitable.java
@@ -0,0 +1,73 @@
+interface Visitable {
+    public void accept(Visitor visitor);
+}
+
+class Book implements Visitable {
+    private double price;
+    private double weight;
+
+    public Book(double price, double weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
+
+class CD implements Visitable {
+    private double price;
+    private double weight;
+
+    public CD(double price, double weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
+
+class DVD implements Visitable {
+    private double price;
+    private double weight;
+
+    public DVD(double price, double weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
+
